70d3b101297a66746201ded2c0ce1b08741f6f2b
IANMWIRIGI99 <mwirigiian75@gmail.com>
2022-09-13 16:38:46 -0700

    one

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Description: Prints the alphabet with _putchar
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char b;
+
+	for (b = 'a'; b <= 'z'; b++)
+
+	{
+		putchar(b);
+	}
+	putchar('\n');
+}
