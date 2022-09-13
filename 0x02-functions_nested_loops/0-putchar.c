36f5bf0ee6363abd12a241b32c33afe74ff248a5
IANMWIRIGI99 <mwirigiian75@gmail.com>
2022-09-13 15:53:04 -0700

    zero

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Prints __putchar
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	char c;
+	char s[] = "_putchar";
+
+	while (i <= 7)
+	{
+		c = s[i];
+		_putchar(c);
+		i++;
+	}
+	_putchar('\n');
+
+return (0);
+}
