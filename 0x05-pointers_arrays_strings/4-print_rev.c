13a0440fce2ea2dabb6702e542909aec7ba2d88c
IANMWIRIGI99 <mwirigiian75@gmail.com>
2022-09-19 13:40:26 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code.
+ * @s: an input string.
+ * Return: nothing.
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (len)
+		_putchar(s[--len]);
+	_putchar('\n');
+}
