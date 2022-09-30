5db5d3155c70a76123982c23fdbc886a3fa1621a
IANMWIRIGI99 <mwirigiian75@gmail.com>
2022-09-30 08:44:05 -0700

    3-print_remaining_days.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,15 +2,16 @@
 #include <stdio.h>
 
 /**
- * main - a program that prints its name, followed by a new line.
- * @argc: arguments' count.
- * @argv: arguments' values.
- * Return: Always 0.
+ * main - Prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always zero
  */
 int main(int argc, char *argv[])
 {
-	if (argc > 0)
-		printf("%s\n", argv[0]);
+	(void) argc;
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
